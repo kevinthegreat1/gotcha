deb055ce4b55d5a8e9e2e3c5e79a05611bb6cc37
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2023-04-20 22:22:27 -0400

    Added firebase query

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import {initializeApp} from "firebase/app";
+import {getAnalytics} from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+import {collection, doc, getDoc, getFirestore} from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -19,3 +20,24 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// Initialize Firestore
+const database = getFirestore();
+const gamesCollection = collection(database, 'games');
+const gameDoc = doc(gamesCollection, "test-game-01");
+
+let player;
+let name;
+let alive;
+let targetEmail;
+
+getDoc(gameDoc).then(snapshot => {
+    player = snapshot.data().players["test1@example.com"];
+    name = player.name;
+    alive = player.alive;
+    targetEmail = player.targetEmail;
+    document.getElementById("alive").innerHTML += alive ? "alive" : "unalive";
+    document.getElementById("target").innerHTML += targetEmail;
+}).catch(error => {
+    console.log(error);
+});
