74bdc356e944ddac9cde7ee7df5295a45aff987c
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2023-04-21 09:24:52 -0400

    Added auth

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import {getAnalytics} from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import {collection, doc, getDoc, getFirestore} from "firebase/firestore";
+import {getAuth, GoogleAuthProvider, signInWithPopup} from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -21,23 +22,35 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
+// Initialize Auth
+const provider = new GoogleAuthProvider();
+const auth = getAuth();
+
+document.getElementById("signIn").onclick = function signIn() {
+    signInWithPopup(auth, provider).then(result => {
+        const user = result.user;
+        const name = result.user.displayName;
+        const email = result.user.email;
+        document.getElementById("name").innerHTML += name;
+        document.getElementById("signIn").style.visibility = "hidden";
+        document.getElementById("name").style.visibility = "visible";
+        document.getElementById("alive").style.visibility = "visible";
+        document.getElementById("target").style.visibility = "visible";
+    });
+}
+
 // Initialize Firestore
 const database = getFirestore();
 const gamesCollection = collection(database, 'games');
 const gameDoc = doc(gamesCollection, "test-game-01");
 
-let player;
-let name;
-let alive;
-let targetEmail;
-
 getDoc(gameDoc).then(snapshot => {
-    player = snapshot.data().players["test1@example.com"];
-    name = player.name;
-    alive = player.alive;
-    targetEmail = player.targetEmail;
+    const player = snapshot.data().players["test1@example.com"];
+    const name = player.name;
+    const alive = player.alive;
+    const targetEmail = player.targetEmail;
     document.getElementById("alive").innerHTML += alive ? "alive" : "unalive";
-    document.getElementById("target").innerHTML += targetEmail;
+    document.getElementById("target").innerHTML += name;
 }).catch(error => {
     console.log(error);
 });
