8cc30b8142d9d98a6e379bcb3d6b58509df57ace
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2024-02-18 19:54:36 -0500

    Add google sign in

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,23 +1,41 @@
-import {SafeAreaView, Text, TouchableOpacity, View} from "react-native";
-import {useState} from "react";
-import {auth, provider} from "../firebaseConfig";
+import {SafeAreaView, View} from "react-native";
+import {useEffect, useState} from "react";
+import {GoogleSignin, GoogleSigninButton} from "@react-native-google-signin/google-signin";
 
 export default function Login() {
-  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState();
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    GoogleSignin.configure();
+  }, []);
+
+  const signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      setUser(await GoogleSignin.signIn());
+      setError();
+    } catch (e) {
+      setError(e);
+    }
+  }
+
+  const logout = async () => {
+    try {
+      await GoogleSignin.revokeAccess();
+      await GoogleSignin.signOut();
+      setUser();
+      setError();
+    } catch (e) {
+      setError(e);
+    }
+  }
 
   return (
     <View>
       <SafeAreaView>
         <View>
-          <TouchableOpacity onPress={() => {
-            setLoading(true);
-          }}>
-            <View>
-              <View>
-                <Text>Login with Google</Text>
-              </View>
-            </View>
-          </TouchableOpacity>
+          <GoogleSigninButton onPress={signIn}/>
         </View>
       </SafeAreaView>
     </View>
